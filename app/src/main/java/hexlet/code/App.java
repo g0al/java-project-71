2ea17e7367904dba26a9c1ff1e57ea570ff065c3
g0al <goalenator@gmail.com>
2024-11-24 15:14:40 +0300

    second commit

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,8 +1,24 @@
 package hexlet.code;
 
-public class App {
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
+
+@Command(name="gendiff", mixinStandardHelpOptions = true, version = "helloWorld 1.0",
+        description = "Compares two configuration files and shows a difference.")
+public class App implements Runnable {
+
+    //@Option(names = "--option", description = "Some option.")
+    //String option;
+
+    @Override
+    public void run() {
+        System.out.println("Hello World!");
+    }
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int exitCode = new CommandLine(new App()).execute(args);
+        System.exit(exitCode);
     }
 }
